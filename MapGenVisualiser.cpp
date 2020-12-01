f3ccbc1774224b3f10933682feef3e569001ba4b
Alexander1248 <75137404+Alexander1248@users.noreply.github.com>
2020-12-01 17:03:11 +0300

    Add files via upload

--- MapGenVisualiser.cpp
+++ MapGenVisualiser.cpp
@@ -9,9 +9,14 @@ using namespace sf;
 
 int main() {
 	Vec2 mapSize;
+	Image map;
 
 	mapSize.x = 1024;
 	mapSize.y = 1024;
+	map.loadFromFile("map.png");
+	mapSize.x = map.getSize().x;
+	mapSize.y = map.getSize().y;
+
 	int hmax = 10000;
 	float maxT = 50; float minT = -10;
 	float waterCoef = 8.5;
@@ -26,7 +31,7 @@ int main() {
 	int state = 0;
 	Font font;
 	font.loadFromFile("123.ttf");
-	Texture map;
+	Texture map1;
 	Sprite img;
 	RectangleShape button(Vector2f({ 200,50 }));
 	RenderWindow window(sf::VideoMode(1280, 720), "WorldGen");
@@ -39,8 +44,8 @@ int main() {
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		map.loadFromFile("map.png");
-		img.setTexture(map);
+		map1.loadFromFile("map.png");
+		img.setTexture(map1);
 		img.setPosition(Vector2f({ 560,0 }));
 		img.setScale(Vector2f({ 720.f / mapSize.x ,720.f / mapSize.y }));
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
@@ -86,6 +91,23 @@ int main() {
 			if (state == 10) rivRandRer -= 0.1;
 			if (state == 11) scale--;
 			if (state == 12) tdat--;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Tab))
+		{
+			if (state == 0) cin >> mapSize.x;
+			if (state == 1) cin >> mapSize.y;
+			if (state == 2) cin >> hmax;
+			if (state == 3) cin >> maxT;
+			if (state == 4) cin >> minT;
+			if (state == 5) cin >> waterCoef;
+			if (state == 6) cin >> waterLevel;
+			if (state == 7) cin >> mountianLevel;
+			if (state == 8) cin >> kRivers;
+			if (state == 9) cin >> riverLen;
+			if (state == 10) cin >> rivRandRer;
+			if (state == 11) cin >> scale;
+			if (state == 12) cin >> tdat;
+			Sleep(100);
 		}
 		if (abs(tdat) % 3 == 0)  type = "Sphere";
 		if (abs(tdat) % 3 == 1)  type = "CoredPlane";
@@ -166,7 +188,6 @@ int main() {
 		if (localPosition.x >= 10 && localPosition.x <= 210 && localPosition.y >= 660 && localPosition.y <= 710) {
 			button.setFillColor(Color::Green);
 			if (Mouse::isButtonPressed(sf::Mouse::Left)) {
-				Image map;
 				Uint8* pix = new Uint8[mapSize.x * mapSize.y * 4];
 				float** data = new float* [4];
 				for (int i = 0; i < 4; i++) data[i] = new float[mapSize.x * mapSize.y];
@@ -175,8 +196,8 @@ int main() {
 				cout << "Creating completed!" << endl;
 				cout << "Saving file..." << endl;
 				for (int i = 0; i < mapSize.x * mapSize.y; i++) {
-					if (float(data[0][i]) < 2000) pix[i * 4] = 0, pix[i * 4 + 1] = 0, pix[i * 4 + 2] = data[0][i] * 255 / 2000, pix[i * 4 + 3] = 255;
-					else pix[i * 4] = data[0][i] * 255 / 10000, pix[i * 4 + 1] = data[0][i] * 255 / 10000, pix[i * 4 + 2] = data[0][i] * 255 / 10000, pix[i * 4 + 3] = 255;
+					if (float(data[0][i]) < waterLevel) pix[i * 4] = 0, pix[i * 4 + 1] = 0, pix[i * 4 + 2] = data[0][i] * 255 / waterLevel, pix[i * 4 + 3] = 255;
+					else pix[i * 4] = data[0][i] * 255 / hmax, pix[i * 4 + 1] = data[0][i] * 255 / hmax, pix[i * 4 + 2] = data[0][i] * 255 / hmax, pix[i * 4 + 3] = 255;
 				}
 				map.create(mapSize.x, mapSize.y, pix);
 				map.saveToFile("hmap.png");
@@ -186,9 +207,9 @@ int main() {
 				map.saveToFile("pmap.png");
 
 				for (int i = 0; i < mapSize.x * mapSize.y; i++) {
-					pix[i * 4] = 4.25 * (data[2][i] + 10);
+					pix[i * 4] = 4.25 * (data[2][i] - minT);
 					pix[i * 4 + 1] = 0;
-					pix[i * 4 + 2] = 4.25 * (50 - data[2][i]);
+					pix[i * 4 + 2] = 4.25 * (maxT - data[2][i]);
 					pix[i * 4 + 3] = 255;
 				}
 				map.create(mapSize.x, mapSize.y, pix);
@@ -210,18 +231,18 @@ int main() {
 						pix[i * 4] = 0, pix[i * 4 + 1] = 0, pix[i * 4 + 2] = 255, pix[i * 4 + 3] = 255;
 					}
 					else {
-						if (float(data[0][i]) < 2000) pix[i * 4] = 0, pix[i * 4 + 1] = 0, pix[i * 4 + 2] = data[0][i] * 255 / 2000, pix[i * 4 + 3] = 255;
-						else pix[i * 4] = data[0][i] * 255 / 10000, pix[i * 4 + 1] = data[0][i] * 255 / 10000, pix[i * 4 + 2] = data[0][i] * 255 / 10000, pix[i * 4 + 3] = 255;
+						if (float(data[0][i]) < waterLevel) pix[i * 4] = 0, pix[i * 4 + 1] = 0, pix[i * 4 + 2] = data[0][i] * 255 / waterLevel, pix[i * 4 + 3] = 255;
+						else pix[i * 4] = data[0][i] * 255 / hmax, pix[i * 4 + 1] = data[0][i] * 255 / hmax, pix[i * 4 + 2] = data[0][i] * 255 / hmax, pix[i * 4 + 3] = 255;
 					}
 				}
 				map.create(mapSize.x, mapSize.y, pix);
 				map.saveToFile("hrmap.png");
 
 				for (int i = 0; i < mapSize.x * mapSize.y; i++) {
-					if (data[3][i] == 1 && data[0][i] >= 2000) {
+					if (data[3][i] == 1 && data[0][i] >= waterLevel) {
 						if (data[2][i] <= 0) {
-							pix[i * 4] = pow(1.6987, -data[2][i]);
-							pix[i * 4 + 1] = pow(1.7405, -data[2][i]);
+							pix[i * 4] = data[2][i] * 200 / minT;
+							pix[i * 4 + 1] = data[2][i] * 255 / minT;
 							pix[i * 4 + 2] = 250;
 						}
 						else {
@@ -231,26 +252,26 @@ int main() {
 						}
 					}
 					else {
-						if (float(data[0][i]) <= 2000) {
+						if (float(data[0][i]) <= waterLevel) {
 							if (data[2][i] <= 0) {
-								pix[i * 4] = pow(1.6987, -data[2][i]);
-								pix[i * 4 + 1] = pow(1.7405, -data[2][i]);
+								pix[i * 4] = data[2][i] * 200 / minT;
+								pix[i * 4 + 1] =  data[2][i] * 255 / minT;
 								pix[i * 4 + 2] = 250;
 							}
 							else {
 								pix[i * 4] = 0;
 								pix[i * 4 + 1] = 0;
-								pix[i * 4 + 2] = data[0][i] * 255 / 2000;
+								pix[i * 4 + 2] = data[0][i] * 255 / waterLevel;
 							}
 						}
 						else {
 							if (data[2][i] <= 0) {
-								pix[i * 4] = (data[0][i] * 255 / 10000 + 255) / 2;
-								pix[i * 4 + 1] = (data[0][i] * 255 / 10000 + 255) / 2;
-								pix[i * 4 + 2] = (data[0][i] * 255 / 10000 + 255) / 2;
+								pix[i * 4] = (data[0][i] * 255 / hmax + 255) / 2;
+								pix[i * 4 + 1] = (data[0][i] * 255 / hmax + 255) / 2;
+								pix[i * 4 + 2] = (data[0][i] * 255 / hmax + 255) / 2;
 							}
 							else {
-								pix[i * 4] = data[2][i] * 255 / 50;
+								pix[i * 4] = data[2][i] * 255 / maxT;
 								pix[i * 4 + 1] = sqrt(data[1][i] * 64.99);
 								pix[i * 4 + 2] = 0;
 							}
